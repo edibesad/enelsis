71f844b0a35b8c0cafded8360632211addcaca14
edibesad <edibesad@gmail.com>
2023-10-16 17:34:14 +0300

    dil sorunu çözüldü

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:enelsis/utils/app_router.dart';
 import 'package:enelsis/utils/themes.dart';
 import 'package:enelsis/views/login_page/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -17,6 +18,12 @@ class MainApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(430, 932),
       builder: (context, child) => GetMaterialApp(
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: const [Locale('en'), Locale('tr')],
+        locale: const Locale('tr'),
         unknownRoute: AppRouter.unknownPage,
         getPages: AppRouter.getPages,
         theme: Themes.customTheme,
