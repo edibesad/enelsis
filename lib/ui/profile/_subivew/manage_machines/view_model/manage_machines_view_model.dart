413aa2fca64801fd290e7305609792b60edcec14
edibesad <edibesad@gmail.com>
2023-11-06 11:17:28 +0300

    completed service integration

--- lib/ui/profile/_subivew/manage_machines/view_model/manage_machines_view_model.dart
+++ lib/ui/profile/_subivew/manage_machines/view_model/manage_machines_view_model.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
-
 import 'package:enelsis/core/base/model/base_view_model.dart';
 import 'package:enelsis/core/constants/navigation/navigation_constants.dart';
-import 'package:enelsis/services/sim_service.dart';
 import 'package:enelsis/product/model/machine_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../../../core/base/model/base_response_model.dart';
 
 class ManageMachinesViewModel extends BaseViewModel {
   RxList<MachineModel> machines = RxList.empty();
@@ -13,21 +12,22 @@ class ManageMachinesViewModel extends BaseViewModel {
 
   @override
   void init() {
-    getMachines();
+    getMachinesRequest();
   }
 
   @override
   void setContext(BuildContext context) => viewModelContext = context;
 
-  getMachines() async {
-    isLoading.value = true;
-    var json = jsonDecode(await SimService().fetchMachines()) as List;
-    machines.value = json.map((e) => MachineModel.fromJson(e)).toList();
-    isLoading.value = false;
-  }
-
   navigateToEdit(MachineModel machine) {
     navigation.navigateToPage(
         path: NavigationConstants.EDIT_MACHINE, data: machine);
+  }
+
+  getMachinesRequest() async {
+    isLoading.value = true;
+    BaseResponseModel<MachineModel> response = await networkManagerInstance
+        .dioGet<MachineModel>("/machines", MachineModel());
+    machines.value = response.dataList!;
+    isLoading.value = false;
   }
 }
