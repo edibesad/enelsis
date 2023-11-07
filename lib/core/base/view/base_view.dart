b37c3a1d6dd642e01bf1b506e72be1d1fb314d9a
edibesad <edibesad@gmail.com>
2023-11-07 14:33:27 +0300

    viewModel.init and set context addet to init state

--- lib/core/base/view/base_view.dart
+++ lib/core/base/view/base_view.dart
@@ -1,7 +1,8 @@
+import 'package:enelsis/core/base/model/base_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class BaseView<T extends GetxController> extends StatefulWidget {
+class BaseView<T extends BaseViewModel> extends StatefulWidget {
   const BaseView(
       {super.key,
       required this.viewModel,
@@ -17,13 +18,15 @@ class BaseView<T extends GetxController> extends StatefulWidget {
   State<BaseView<T>> createState() => _BaseViewState<T>();
 }
 
-class _BaseViewState<T extends GetxController> extends State<BaseView<T>> {
+class _BaseViewState<T extends BaseViewModel> extends State<BaseView<T>> {
   late T viewModel;
   @override
   void initState() {
     super.initState();
     this.viewModel = Get.put(widget.viewModel);
     if (widget.onModelReady != null) widget.onModelReady!(widget.viewModel);
+    viewModel.init();
+    viewModel.setContext(context);
   }
 
   @override
